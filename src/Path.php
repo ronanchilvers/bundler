6f8aa1c65795db38876e9d224d37c412eced21c8
Ronan Chilvers <ronan@d3r.com>
2025-09-23 20:47:36 +0100

    Add Path class for handling path placeholders and storage

---
+++ src/Path.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ronanchilvers\Bundler;
+
+class Path
+{
+    public static function placeholders(
+        string $path,
+    ): string {
+        $root = null;
+        $thisDir = './';
+        while (true) {
+            $test = realpath(__DIR__ . DIRECTORY_SEPARATOR . $thisDir . 'vendor');
+            if ('/' == $test) {
+                break;
+            }
+            if ($test && is_dir($test)) {
+                $root = dirname($test);
+                break;
+            }
+            $thisDir .= '../';
+        }
+        $replacements = [
+            '{root}' => $root,
+        ];
+        return str_replace(
+            array_keys($replacements),
+            array_values($replacements),
+            $path,
+        );
+    }
+
+    protected $data = [];
+
+    public function __construct(string $realpath, string $path)
+    {
+        $this->data = [
+        'realpath' => $realpath,
+        'path' => $path,
+        ];
+    }
+
+    public function realpath(): string
+    {
+        return $this->data['realpath'];
+    }
+
+    public function setRealpath(string $realpath): static
+    {
+        $this->data['realpath'] = $realpath;
+
+        return $this;
+    }
+
+    public function path(): string
+    {
+        return $this->data['path'];
+    }
+
+    public function setPath(string $path): static
+    {
+        $this->data['path'] = $path;
+
+        return $this;
+    }
+}
